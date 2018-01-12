fdd37eae7a0086cd3389c239cc60f6bdeb72b631
CJ Haviland <cj.haviland@yahoo.com>
2018-01-12 21:19:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var animal = 'dog'
+var animal = 'dog';
 
-function myAnimal() {
-  return animal
+function myAnimal(yourAnimal) {
+  var animal = yourAnimal;
+  return animal;
 }
 
 function yourAnimal() {
